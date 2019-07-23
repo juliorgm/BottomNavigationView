da3f8c1ab740454387450f42177635caae28ea5c
Julio Gomes <juliorgm@gmail.com>
2019-07-23 01:07:46 -0300

    teste

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.myapplication.fragments.BlankFragment;
+import com.example.myapplication.fragments.BlueFragment;
+import com.example.myapplication.fragments.RedFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    getSupportActionBar().setTitle("RedFragment");
+                    openFragment(new RedFragment());
+                    return true;
+                case R.id.navigation_dashboard:
+                    getSupportActionBar().setTitle("RedFragment");
+                    openFragment(new BlueFragment());
+                    return true;
+                case R.id.navigation_notifications:
+                    getSupportActionBar().setTitle("RedFragment");
+                    openFragment(new BlankFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+}
